7a9d6cd1442d866af89ba20a81bf0906ca6caeb9
Polina Simonenko <rabarbrablad@gmail.com>
2024-02-22 10:52:00 +0100

    improvemenrts

--- srcs/backend/transcendence_backend/pong/consumers.py
+++ srcs/backend/transcendence_backend/pong/consumers.py
@@ -18,14 +18,19 @@ class PongConsumer(AsyncWebsocketConsumer):
 	x = 50
 	y = 50
 	angle = 45
-	platform_1 = 50
-	platform_2 = 50
+	platform_1 = 40
+	platform_2 = 40
+	score_1 = 10
+	score_2 = 10
+	p1_change = False
+	p2_change = False
+	score_change = False
 
 	async def connect(self):
 		if len(self.channel_layer.groups.get('game', {}).keys()) < 2:
 			await self.channel_layer.group_add('game', self.channel_name)
 			await self.accept()
-		if len(self.channel_layer.groups.get('game', {}).keys()) == 1:
+		if len(self.channel_layer.groups.get('game', {}).keys()) == 2:
 			await self.send(json.dumps({'side':'right'}))
 			self.task = asyncio.ensure_future(self.send_updates())
 
@@ -59,43 +64,64 @@ class PongConsumer(AsyncWebsocketConsumer):
 		self.y += math.sin(self.angle)
 		if dir_left == 'up' and self.platform_1 > 0:
 			self.platform_1 -= 2
+			self.p1_change = True
 		elif dir_left == 'down' and self.platform_1 < 80:
 			self.platform_1 += 2
+			self.p1_change = True
 		if dir_right == 'up' and self.platform_2 > 0:
 			self.platform_2 -= 2
+			self.p2_change = True
 		elif dir_right == 'down' and self.platform_2 < 80:
 			self.platform_2 += 2
+			self.p2_change = True
 
 	async def check_collisions(self):
-		if self.x <= 2:
-			if self.platform_1 < self.y < self.platform_1 + 20:
+		if self.x <= 1:
+			if self.platform_1 - 1 < self.y < self.platform_1 + 21:
 				self.angle = self.angle - 180
+				self.score_1 += 1
 			else:
 				self.x = 97
-		elif self.x >= 98:
-			if self.platform_2 < self.y < self.platform_2 + 20:
+				self.score_1 -= 1
+			self.score_change = True
+		elif self.x >= 99:
+			if self.platform_2 - 1 < self.y < self.platform_2 + 21:
 				self.angle = self.angle - 180
+				self.score_2 += 1
 			else:
 				self.x = 3
-		elif self.y <= 2 or self.y >= 98:
+				self.score_2 -= 1
+			self.score_change = True
+		elif self.y <= 1 or self.y >= 99:
 			self.angle = -self.angle
 
 	async def send_updates(self):
 		while True:
 			await self.check_collisions()
 			await self.move()
+			data = {
+				'x': round(self.x, 2),
+              	'y': round(self.y, 2),
+			}
+			if self.p1_change:
+				data['p1'] = int(self.platform_1)
+			if self.p2_change:
+				data['p2'] = int(self.platform_2)
+			if self.score_change:
+				data.update({
+					's1': self.score_1,
+					's2': self.score_2,
+				})
 			await self.channel_layer.group_send(
 				'game',
        			{
               		"type": "game.update",
-              		"text": json.dumps({
-            			'x': round(self.x, 2),
-              			'y': round(self.y, 2),
-                		'p1': int(self.platform_1),
-                	 	'p2': int(self.platform_2)
-                	})
+              		"text": json.dumps(data)
 				}
           	)
+			self.p1_change = False
+			self.p2_change = False
+			self.score_change = False
 			await asyncio.sleep(0.01)
    
 	async def game_update(self, event):
--- srcs/frontend/pong.js
+++ srcs/frontend/pong.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	const field = document.getElementById('field');
+function runGame() {
+    const field = document.getElementById('field');
     const ball = document.getElementById('ball');
     const platform_1 = document.getElementById('platform_1');
     const platform_2 = document.getElementById('platform_2');
     const scoreboard = document.getElementById('scoreboard');
     const ws = new WebSocket('ws://localhost:8000/ws');
     
-    let ballX = field.clientWidth / 2 + field.offsetLeft;
-    let ballY = field.offsetTop + 5;
+    let ballX = 50;
+    let ballY = 50;
     
-    let platformY_1 = field.clientHeight / 2 - 100;
-    let platformY_2 = field.clientHeight / 2  - 100;
+    let platformY_1 = 40;
+    let platformY_2 = 40;
     
     let score_1 = 10;
     let score_2 = 10;
@@ -49,33 +49,40 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 
     function gameLoop() {
-        ball.style.left = `${ballX - 10}px`;
-        ball.style.top = `${ballY - 10}px`;
-        platform_1.style.top = `${platformY_1 + 2}px`;
-        platform_2.style.top = `${platformY_2 + 2}px`;
+        ball.style.left = `${ballX - 1}%`;
+        ball.style.top = `${ballY - 1}%`;
+        platform_1.style.top = `${platformY_1}%`;
+        platform_2.style.top = `${platformY_2}%`;
+        updateScoreBoard();
         requestAnimationFrame(gameLoop);
     }
-    let dx = field.clientWidth / 100;
-    let dy = field.clientHeight / 100;
-    platform_1.style.height = `${dy * 20}px`;
-    platform_2.style.height = `${dy * 20}px`;
     addEventListener("resize", (event) => {
-        dx = field.clientWidth / 100;
-        dy = field.clientHeight / 100;
-        platform_1.style.height = `${dy * 20}px`;
-        platform_2.style.height = `${dy * 20}px`;
+        platform_1.style.height = "20%";
+        platform_2.style.height = "20%";
     });
     ws.addEventListener('message', ev => {
         const data = JSON.parse(ev.data);
         if (data.side === 'right')
             me = 'right'
-        ballX = field.offsetLeft + dx * data.x;
-        ballY = field.offsetTop + dy * data.y;
-        platformY_1 = field.offsetTop + dy * data.p1;
-        platformY_2 = field.offsetTop + dy * data.p2;
+        ballX = data.x;
+        ballY = data.y;
+        if ('p1' in data)
+            platformY_1 = data.p1;
+        if ('p2' in data)
+            platformY_2 = data.p2;
+        if ('s1' in data)
+        {
+            score_1 = data.s1;
+            score_2 = data.s2;
+        }
     })
     gameLoop();
 	
     document.addEventListener('keydown', handleKeyPress);
     document.addEventListener('keyup', handleKeyRelease);
+}
+
+document.addEventListener("readystatechange", function(event) {
+    if (event.target.readyState === "complete")
+    	runGame();
 })
