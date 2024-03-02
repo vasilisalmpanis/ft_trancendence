2ce86db4590fd00a10e0059c677af4851df9da87
valmpani <vasilisalmpanis@gmail.com>
2024-03-02 19:29:15 +0100

    better errors for friend request class and small fixes

--- srcs/backend/transcendence_backend/users/models/friend_request.py
+++ srcs/backend/transcendence_backend/users/models/friend_request.py
@@ -1,3 +1,5 @@
+from re import U
+from sys import exception
 from typing                     import Any
 from django.db                  import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
@@ -12,7 +14,7 @@ class FriendRequest(models.Model):
     message = models.CharField(max_length=255, null=True, blank=True)
     status = models.CharField(max_length=255, default="PENDING")
 
-    def get_user_friend_requests(user_id : int, type : str) -> list:
+    def get_user_friend_requests(self, user_id : int, type : str) -> list:
         if type == "sent":
             friend_requests = FriendRequest.objects.filter(sender__id=user_id).filter(status="PENDING")
         else:
@@ -26,17 +28,19 @@ class FriendRequest(models.Model):
             for friend_request in friend_requests
         ]
     
-    def create_friend_request(sender_id : int, receiver_id : int, message : str = None) -> Any:
+    def create_friend_request(self, sender_id: int, receiver_id: int, message: Any = None) -> Any:
         if sender_id == receiver_id:
-            return None
+            raise Exception("You cannot send a friend request to yourself")
         if not User.objects.filter(id=sender_id).exists():
-            return None
+            raise Exception("Sender does not exist")
         elif not User.objects.filter(id=receiver_id).exists():
-            return None
+            raise Exception("Receiver does not exist")
         if not message:
             message = f"{User.objects.get(id=sender_id).username} wants to be your friend"
+        if User.objects.get(id=sender_id).friends.filter(id=receiver_id).exists():
+            raise Exception("You are already friends")
         if FriendRequest.objects.filter(sender_id=sender_id, receiver_id=receiver_id).exists():
-            return None
+            raise Exception("You already sent a friend request to this user")
         friend_request = FriendRequest.objects.create(
             sender_id=sender_id,
             receiver_id=receiver_id,
@@ -44,12 +48,12 @@ class FriendRequest(models.Model):
         )
         return friend_request
     
-    def accept_friend_request(request_id : int, user) -> bool:
+    def accept_friend_request(self, request_id : int, user) -> bool:
         friend_request = FriendRequest.objects.filter(id=request_id).filter(receiver=user).first()
         if not friend_request:
-            return False
+            raise Exception("Friend request does not exist")
         if friend_request.status != "PENDING":
-            return False
+            raise Exception("Friend request is not pending")
         sender = friend_request.sender
         receiver = friend_request.receiver
         sender.friends.add(receiver)
@@ -58,23 +62,23 @@ class FriendRequest(models.Model):
         friend_request.save()
         return True
     
-    def decline_friend_request(request_id : int, user) -> bool:
+    def decline_friend_request(self, request_id : int, user) -> bool:
         friend_request = FriendRequest.objects.filter(id=request_id).filter(receiver=user).first()
         if not friend_request:
-            return False
+            raise Exception("Friend request does not exist")
         if friend_request.status != "PENDING":
-            return False
+            raise Exception("Friend request is not pending")
         friend_request.status = "DECLINED"
         friend_request.save()
         return True
         
-    def delete_friend_request(request_id : int) -> bool:
+    def delete_friend_request(self, request_id : int) -> bool:
         friend_request = FriendRequest.objects.filter(id=request_id).first()
         if friend_request:
             friend_request.delete()
             return True
         else:
-            return False
+            raise Exception("Friend request does not exist")
     class Meta:
         verbose_name = 'Friend Request'
         verbose_name_plural = 'Friend Requests'
