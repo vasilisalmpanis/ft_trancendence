ff9f7c9aa87e2db8b4b8f36f332fc8a71cff89ca
Polina Simonenko <rabarbrablad@gmail.com>
2024-04-07 19:23:28 +0200

    fix: ft_react render bug catched

--- srcs/frontend/app/srcs/components/barlayout.jsx
+++ srcs/frontend/app/srcs/components/barlayout.jsx
@@ -2,32 +2,28 @@ import ftReact		from "../ft_react";
 import Layout from "./layout";
 
 
-const BarLayout = (props) => {
-	const children = props.children;
-	props.children = [];
-	return (
-		<div className="h-100">
-			<nav className="navbar bg-body-tertiary">
-			  	<div className="container-fluid">
-				  	<button
-						onClick={() => props.route("/")}
-						className="btn btn-outline-secondary me-3"
-					>
-						PONG 42
-					</button>
-					<button
-						onClick={() => props.route("/me")}
-						className="rounded-circle btn btn-outline-secondary me-3 ms-auto"
-					>
-						P
-					</button>
-			  	</div>
-			</nav>
-			<Layout>
-				{children}
-			</Layout>
-		</div>
-	)
-};
+const BarLayout = (props) => (
+	<div className="h-100">
+		<nav className="navbar bg-body-tertiary">
+		  	<div className="container-fluid">
+			  	<button
+					onClick={() => props.route("/")}
+					className="btn btn-outline-secondary me-3"
+				>
+					PONG 42
+				</button>
+				<button
+					onClick={() => props.route("/me")}
+					className="rounded-circle btn btn-outline-secondary me-3 ms-auto"
+				>
+					P
+				</button>
+		  	</div>
+		</nav>
+		<Layout>
+			{props.children}
+		</Layout>
+	</div>
+);
 
 export default BarLayout; 
--- srcs/frontend/app/srcs/components/layout.jsx
+++ srcs/frontend/app/srcs/components/layout.jsx
@@ -1,22 +1,18 @@
 import ftReact from "../ft_react";
 
-const Layout = (props) => {
-	const children = props.children;
-	props.children = [];
-	return (
-		<div className="
-			container-md
-			text-center
-			d-flex
-			flex-column
-			mb-3
-			justify-content-center
-			align-items-center
-			h-100
-		">
-			{children}
-		</div>
-	);
-};
+const Layout = (props) => (
+	<div className="
+		container-md
+		text-center
+		d-flex
+		flex-column
+		mb-3
+		justify-content-center
+		align-items-center
+		h-100
+	">
+		{props.children}
+	</div>
+);
 
 export default Layout;
